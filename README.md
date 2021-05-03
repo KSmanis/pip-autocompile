51ddfe3513c582575ab52de87b4485833b2394a2
Konstantinos Smanis <konstantinos.smanis@gmail.com>
2021-05-04 00:02:44 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# pip-autocompile
+
+[![Renovate enabled](https://img.shields.io/badge/renovate-enabled-brightgreen.svg)](https://renovatebot.com/)
+[![pre-commit](https://github.com/KSmanis/pip-autocompile/workflows/pre-commit/badge.svg)](https://github.com/KSmanis/pip-autocompile/actions?workflow=pre-commit)
+[![super-linter](https://github.com/KSmanis/pip-autocompile/workflows/super-linter/badge.svg)](https://github.com/KSmanis/pip-autocompile/actions?workflow=super-linter)
+[![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)
+[![Imports: isort](https://img.shields.io/badge/%20imports-isort-%231674b1?style=flat&labelColor=ef8336)](https://pycqa.github.io/isort/)
+[![security: bandit](https://img.shields.io/badge/security-bandit-yellow.svg)](https://github.com/PyCQA/bandit)
+
+Automate pip-compile for multiple environments.
---
+++ pipautocompile/cli.py
@@ -0,0 +1,113 @@
+import logging
+import os
+import re
+import shlex
+import subprocess  # nosec
+import sys
+from os import PathLike
+from pathlib import Path
+from typing import AnyStr, Iterable, List, Optional, Union
+
+import click
+from python_on_whales import docker
+
+DEFAULT_BUILD_STAGE = "build-deps"
+DEFAULT_PIP_COMPILE_ARGS = (
+    "--allow-unsafe --generate-hashes --no-reuse-hashes --upgrade"
+)
+
+logging.basicConfig(level=logging.INFO)
+
+
+def _find_spec_files(
+    path: Union[str, bytes, PathLike] = ".",
+    patterns: Iterable[str] = (
+        "**/requirements.in",
+        "**/requirements/*.in",
+    ),
+) -> List[Path]:
+    return sorted(s for p in patterns for s in Path(path).glob(p) if s.is_file())
+
+
+def _file_contains_regex(
+    file: Union[str, bytes, PathLike],
+    regex: AnyStr,
+    flags: Union[int, re.RegexFlag] = 0,
+) -> bool:
+    try:
+        f = open(file)
+    except OSError:
+        pass
+    else:
+        with f:
+            for line in f:
+                if re.search(regex, line, flags) is not None:
+                    return True
+    return False
+
+
+@click.command(context_settings={"help_option_names": ("-h", "--help")})
+@click.version_option()
+@click.option(
+    "--build-stage",
+    help="Docker build stage to search for.",
+    default=DEFAULT_BUILD_STAGE,
+    show_default=True,
+)
+@click.option(
+    "--pip-compile-args",
+    help="Arguments to pass on to pip-compile.",
+    default=DEFAULT_PIP_COMPILE_ARGS,
+    show_default=True,
+)
+def cli(build_stage: Optional[str], pip_compile_args: Optional[str]):
+    """Automate pip-compile for multiple environments."""
+
+    for spec in _find_spec_files():
+        logging.info(f"Processing '{spec}'...")
+        basename_in = spec.name
+        basename_out = spec.with_suffix(".txt").name
+        spec_dir = spec.resolve(strict=True).parent
+        build_dir = spec_dir if basename_in == "requirements.in" else spec_dir.parent
+
+        args = " ".join(map(shlex.quote, sys.argv[1:]))
+        env = {"CUSTOM_COMPILE_COMMAND": f"pip-autocompile {args}"}
+        has_build_stage = _file_contains_regex(
+            file=build_dir / "Dockerfile",
+            regex=fr"^FROM \S+ AS {build_stage}$",
+        )
+        if has_build_stage:
+            image_id = docker.build(context_path=build_dir, target=build_stage).id
+            docker.run(
+                image=image_id,
+                command=[
+                    "sh",
+                    "-c",
+                    f"""
+                        set -eux;
+                        python -m venv /opt/venv/;
+                        /opt/venv/bin/pip install pip-tools;
+                        /opt/venv/bin/pip-compile {pip_compile_args} '{basename_in}';
+                        chown --reference '{basename_in}' '{basename_out}';
+                        chmod --reference '{basename_in}' '{basename_out}';
+                    """,
+                ],
+                envs=env,
+                remove=True,
+                volumes=[
+                    (spec_dir, "/app/"),
+                    ("pip-autocompile-cache-pip", "/root/.cache/pip/"),
+                    ("pip-autocompile-cache-pip-tools", "/root/.cache/pip-tools/"),
+                ],
+                workdir="/app/",
+            )
+        else:
+            subprocess.check_call(  # nosec
+                ["pip-compile", *pip_compile_args.split(), basename_in],
+                cwd=spec_dir,
+                env={**os.environ, **env},
+            )
+
+
+if __name__ == "__main__":
+    cli()
