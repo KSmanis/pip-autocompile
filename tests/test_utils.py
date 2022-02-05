a5445bc9e0efde3837e0cd1d39237bc52f8116a8
Konstantinos Smanis <konstantinos.smanis@gmail.com>
2022-02-05 18:22:30 +0200

    test: introduce unit tests

---
+++ tests/test_utils.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+
+import pytest
+
+from pipautocompile.utils import quote_args
+
+
+@pytest.mark.parametrize(
+    argnames=("args", "expected"),
+    argvalues=(
+        (tuple(), ""),
+        (("",), "''"),
+        (("", ""), "'' ''"),
+        (("foo",), "foo"),
+        (("foo", "bar"), "foo bar"),
+        (("foo", "bar", "baz"), "foo bar baz"),
+        ((" ",), "' '"),
+        ((" foo",), "' foo'"),
+        ((" foo ",), "' foo '"),
+        ((" foo", "bar"), "' foo' bar"),
+        ((" foo", "bar "), "' foo' 'bar '"),
+        ((" foo ", "bar "), "' foo ' 'bar '"),
+        ((" foo ", " bar "), "' foo ' ' bar '"),
+        ((" foo ", " bar ", " baz "), "' foo ' ' bar ' ' baz '"),
+    ),
+    ids=lambda argvalue: repr(argvalue),
+)
+def test_quote_args(args: tuple[str], expected: str) -> None:
+    assert quote_args(*args) == expected
